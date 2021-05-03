6cd30723d4c4bdcd2f41057a587f90b788f63848
Vladimir <alfa.voland@gmail.com>
2021-05-03 17:27:16 +0300

    constructor to the class to initialize the state

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+
   render() {
     return (
       <button className="square" onClick={() => alert('клик')}>
